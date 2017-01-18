de3970b5d107530b34de7833dcb1a6f84f5ade98
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-18 13:02:15 -0600

    changed axios.baseURL.

--- src/helpers/axiosConfigs.js
+++ src/helpers/axiosConfigs.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8000/api/v1';
+axios.defaults.baseURL = 'api/v1';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
