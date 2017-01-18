765efe6c9f65db11e623dd47bd9c5451c8484709
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-18 11:45:53 -0600

    added client code. setup new scripts. runs locally.

--- api/index.js
+++ api/index.js
@@ -17,6 +17,7 @@ const mode = process.env.NODE_ENV;
 const app = express();
 
 // Middleware
+app.use(express.static('build'));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', port);
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
@@ -37,6 +38,9 @@ app.use(compression());
 
 // Routing
 app.use('/api/v1', [photoRoutes, userRoutes]);
+app.get('/*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // Express Server
 export const server = Server(app);
