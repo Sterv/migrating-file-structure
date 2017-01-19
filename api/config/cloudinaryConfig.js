1db04f50d895ad19bcef8438ecb0e76d75bd51ff
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-19 12:08:23 -0600

    file structure rebuilt.

--- api/config/cloudinaryConfig.js
+++
@@ -1,7 +0,0 @@
-import cloudinary from 'cloudinary';
-
-cloudinary.config({
-  cloud_name: process.env.REACT_APP_CLOUD_NAME,
-  api_key: process.env.REACT_APP_API_KEY,
-  api_secret: process.env.REACT_APP_API_SECRET
-});
--- src/helpers/axiosConfigs.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.baseURL = 'api/v1';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
