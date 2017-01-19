f32176f1f83e0781430349c497efc4958ae7594e
Cody Brunner <rockchalkwushock@icloud.com>
2017-01-19 13:03:14 -0600

    migrated api code.

---
+++ api/index.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import { Server } from 'http';
+import path from 'path';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
+import compression from 'compression';
+import passport from 'passport';
+import mongoose from 'mongoose';
+import { photoRoutes, userRoutes } from './modules';
+
+// ENVIRONMENT VARIABLES
+const port = process.env.PORT || 8000;
+const mode = process.env.NODE_ENV; // set in script or in Heroku.
+let db;
+
+const app = express();
+
+// MIDDLEWARE
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', port);
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials'); // eslint-disable-line
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+app.use((err, req, res, next) => {
+  if (res.headersSent) next(err);
+  res.status(err.status || port).render('500');
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(cors());
+app.use(passport.initialize());
+app.use(compression());
+
+// SERVER ROUTING
+app.use('/api/v1', [photoRoutes, userRoutes]);
+
+// NODE ENVIRONMENT SETUP
+if (mode !== 'production') {
+  // NON-PRODUCITON
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('../webpack.config');
+
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+  db = 'mongodb://localhost/photography-dev';
+} else {
+  // PRODUCTION
+  app.use(express.static('dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+  db = process.env.MONGO_URL;
+}
+
+// DATABASE
+mongoose.Promise = global.Promise;
+mongoose.connect(db);
+mongoose.connection
+  .once('open', () => console.log(`Connected to MongoDb: running on ${db}`))
+  .on('error', err => console.warn('Warning', err));
+
+
+// EXPRESS SERVER
+export const server = Server(app);
+server.listen(port, err => {
+  if (err) console.log(`Error happned: ${err}`);
+  console.log(`Server listening on http://localhost:${port} in ${mode}.`);
+});
